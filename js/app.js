2a471083ccc6be06dfd6b46b508b4cc56a0cfd72
Eric Shantz <shan0255@algonquinlive.com>
2021-03-30 11:59:57 -0400

    Add successful search and display cards

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@ import {
   createStore,
 } from "https://cdn.jsdelivr.net/npm/idb-keyval@5/dist/esm/index.js";
 
+
 const APP = {
   BASE_URL: "https://api.themoviedb.org/3/",
   IMG_URL: "https://image.tmdb.org/t/p/",
@@ -26,7 +27,7 @@ const APP = {
   sw: null,
   db: null,
   movieStore: null,
-  dbVersion: 4,
+  dbVersion: 3,
 
   init() {
     if ("serviceWorker" in navigator) {
@@ -48,69 +49,40 @@ const APP = {
 
   pageLoaded() {
     console.log("IM A PAGE AND IM LOADED");
-    //page has just loaded and we need to check the queryString
-    //based on the querystring value(s) run the page specific tasks
-    // console.log('page loaded and checking', location.search);
-    let params = new URL(document.location).searchParams;
-    let keyword = params.get("keyword");
-    if (keyword) {
-      //means we are on results.html
-      console.log(`on results.html - startSearch(${keyword})`);
-      APP.startSearch(keyword);
+    
+    function getParameterByName(name) {
+      var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+      return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
     }
-    let mid = parseInt(params.get("movie_id"));
-    let ref = params.get("ref");
-    if (mid && ref) {
-      //we are on suggest.html
-      console.log(`look in db for movie_id ${mid} or do fetch`);
-      APP.startSuggest({ mid, ref });
+    
+    let tx = APP.createTX('movieStore' , "readonly")
+    tx.oncomplete = (ev)=>{
+      console.log("transaction complete" , ev)
     }
 
+    let store = tx.objectStore("movieStore");
+    let request = store.getAll(getParameterByName("keyword"));
 
-    // APP.saveMovieToDB(keyword);
-    //call a fetch function
+    request.onsuccess = (ev)=>{
+      console.log("successfully got a thing from db, im going to call build list now uwu")
+      let arr = ev.target.result
+      console.log(arr)
+
+      APP.buildList(arr);
+    }
   },
 
   addListeners() {
-    //TODO:
-    //listen for on and off line events
     document.getElementById("searchForm").addEventListener("submit", (ev) => {
-      ev.preventDefault(); //is this actually doing anything?
+      ev.preventDefault();
       APP.formSubmission(ev);
     });
 
-    //TODO:
-    //listen for Chrome install prompt
-    //handle the deferredPrompt
-
-    //listen for sign that app was installed
     window.addEventListener("appinstalled", (evt) => {
       console.log("app was installed");
     });
 
-    //listen for submit of the search form
-    // let searchForm = document.searchForm;
-    // if (searchForm) {
-    //   document.searchForm.addEventListener('submit', (ev) => {
-    //     ev.preventDefault();
-    //     //build the queryString and go to the results page
-    //     let searchInput = document.getElementById('search');
-    //     let keyword = searchInput.value.trim();
-    //     if (keyword) {
-    //       let base = location.origin;
-    //       let url = new URL('./results.html', base);
-    //       url.search = '?keyword=' + encodeURIComponent(keyword);
-    //       location.href = url;
-    //     }
-    //   });
-    // }
-
-    //listen for the click of movie div
-    //to handle clicks of the suggest a movie buttons
-    let movies = document.querySelector(".movies");
     if (movies) {
-      //navigate to the suggested page
-      //build the queryString with movie id and ref title
       movies.addEventListener("click", (ev) => {
         ev.preventDefault();
         let anchor = ev.target;
@@ -127,71 +99,15 @@ const APP = {
     }
   },
 
-  // sendMessage(msg, target) {
-  //   //TODO:
-  //   //send a message to the service worker
-  // },
+  sendMessage(msg, target) {
+    //TODO:
+    //send a message to the service worker
+  },
 
-  // onMessage({ data }) {
-  //   //TODO:
-  //   //message received from service worker
-  // },
-
-  // startSearch(keyword) {
-  //   //TODO: check in IDB for movie results
-  //   if (keyword) {
-  //     //check the db (movie store)
-  //     //if no matches make a fetch call to TMDB API
-  //     //or make the fetch call and intercept it in the SW
-  //     let url = `${APP.BASE_URL}search/movie?api_key=${APP.API_KEY}&query=${keyword}`;
-
-  //     APP.getData(url, (data) => {
-  //       //this is the CALLBACK to run after the fetch
-  //       APP.results = data.results;
-  //       APP.useSearchResults(keyword);
-  //     });
-  //   }
-  // },
-
-  // useSearchResults(keyword) {
-  //   //after getting fetch or db results
-  //   //display search keyword in title
-  //   //then call buildList
-  //   let movies = APP.results;
-  //   let keywordSpan = document.querySelector('.ref-keyword');
-  //   if (keyword && keywordSpan) {
-  //     keywordSpan.textContent = keyword;
-  //   }
-  //   APP.buildList(movies);
-  // },
-
-  // startSuggest({ mid, ref }) {
-  //   //TODO: Do the search of IndexedDB for matches
-  //   //if no matches to a fetch call to TMDB API
-  //   //or make the fetch call and intercept it in the SW
-
-  //   let url = `${APP.BASE_URL}movie/${mid}/similar?api_key=${APP.API_KEY}&ref=${ref}`;
-  //   //TODO: choose between /similar and /suggested endpoints from API
-
-  //   APP.getData(url, (data) => {
-  //     //this is the callback that will be used after fetch
-  //     APP.suggestedResults = data.results;
-  //     APP.useSuggestedResults(ref);
-  //   });
-  // },
-
-  // useSuggestedResults(ref) {
-  //   //after getting fetch/db results
-  //   //display reference movie name in title
-  //   //then call buildList
-  //   let movies = APP.suggestedResults;
-  //   let titleSpan = document.querySelector('#suggested .ref-movie');
-  //   console.log('ref title', ref);
-  //   if (ref && titleSpan) {
-  //     titleSpan.textContent = ref;
-  //   }
-  //   APP.buildList(movies);
-  // },
+  onMessage({ data }) {
+    //TODO:
+    //message received from service worker
+  },
 
   async getData(keyword) {
     console.log("YOU ARE TRYING TO GET SOME DATA SOONNN");
@@ -213,76 +129,76 @@ const APP = {
     } catch (err) {
       console.warn(err);
     }
-
-    APP.pageLoaded();
-
-    // fetch(url)
-    //   .then((resp) => {
-    //     if (resp.ok) {
-    //       return resp.json();
-    //     } else {
-    //       let msg = resp.statusText;
-    //       throw new Error(`Could not fetch movies. ${msg}.`);
-    //     }
-    //   })
-    //   .then((data) => {
-    //     //callback
-    //     cb(data);
-    //     console.log("DATA.RESULTS : " , data.results)
-    //   })
-    //   .catch((err) => {
-    //     console.warn(err);
-    //     cb({ code: err.code, message: err.message, results: [] });
-    //   });
   },
 
-  // buildList: (movies) => {
-  //   console.log("IM BUILDING A LIST")
-  //   location.replace("/results.html");
-  //   //build the list of cards inside the current page
-  //   let movieDiv = document.querySelector(".movies");
-  //   movieDiv.innerHTML = "";
-  //   console.log("MOVIES!", movies);
+  checkDB(keyword){
+    let tx = APP.createTX('movieStore' , "readonly")
+    tx.oncomplete = (ev)=>{
+      console.log("db check completed" , ev)
+    }
 
-  //   console.log(`show ${movies.length} cards`);
-  //   let container = document.querySelector(`.movies`);
-  //   //TODO: customize this HTML to make it your own
-  //   if (container) {
-  //     if (movies.length > 0) {
-  //       container.innerHTML = movies
-  //         .map((obj) => {
-  //           let img = "./img/icon-512x512.png";
-  //           if (obj.poster_path != null) {
-  //             img = APP.IMG_URL + "w500/" + obj.poster_path;
-  //           }
-  //           return `<div class="card hoverable large" data-id="${obj.id}">
-  //         <div class="card-image">
-  //           <img src="${img}" alt="movie poster" class="notmaterialboxed"/>
-  //           </div>
-  //         <div class="card-content activator">
-  //           <h3 class="card-title"><span>${obj.title}</span><i class="material-icons right">more_vert</i></h3>
-  //         </div>
-  //         <div class="card-reveal">
-  //           <span class="card-title grey-text text-darken-4">${obj.title}<i class="material-icons right">close</i></span>
-  //           <h6>${obj.release_date}</h6>
-  //           <p>${obj.overview}</p>
-  //         </div>
-  //         <div class="card-action">
-  //           <a href="#" class="find-suggested light-blue-text text-darken-3">Show Similar<i class="material-icons right">search</i></a>
-  //         </div>
-  //       </div>`;
-  //         })
-  //         .join("\n");
-  //     } else {
-  //       //no cards
-  //       container.innerHTML = `<div class="card hoverable">
-  //         <div class="card-content">
-  //           <h3 class="card-title activator"><span>No Content Available.</span></h3>
-  //         </div>
-  //       </div>`;
-  //     }
-  //   }
-  // },
+    let store = tx.objectStore("movieStore");
+    let request = store.getAll(keyword);
+
+    request.onsuccess = (ev)=>{
+      console.log("successfully got a thing from db, im going to call build list now uwu")
+      let arr = ev.target.result
+      console.log(arr)
+      // APP.buildList(arr)
+    }
+
+  },
+
+  buildList(movies){
+    console.log(movies)
+    let movieRes = movies[0].results
+    let movieResults = movieRes.results
+
+    console.log("IM BUILDING A LIST")
+    let movieDiv = document.querySelector(".movies");
+    movieDiv.innerHTML = "";
+    console.log("MOVIES!", movies);
+
+    console.log(`show ${movieResults.length} cards`);
+    let container = document.querySelector(`.movies`);
+    if (container) {
+      if (movieResults.length > 0) {
+        container.innerHTML = movieResults
+          .map((obj) => {
+            let img = "./img/icon-512x512.png";
+            if (obj.poster_path != null) {
+              img = APP.IMG_URL + "w500/" + obj.poster_path;
+            } else{
+              console.log("add image plz :(")
+            }
+            return `<div class="card hoverable large" data-id="${obj.id}">
+          <div class="card-image">
+            <img src="${img}" alt="movie poster" class="notmaterialboxed"/>
+            </div>
+          <div class="card-content activator">
+            <h3 class="card-title"><span>${obj.title}</span><i class="material-icons right">more_vert</i></h3>
+          </div>
+          <div class="card-reveal">
+            <span class="card-title grey-text text-darken-4">${obj.title}<i class="material-icons right">close</i></span>
+            <h6>${obj.release_date}</h6>
+            <p>${obj.overview}</p>
+          </div>
+          <div class="card-action">
+            <a href="#" class="find-suggested light-blue-text text-darken-3">Show Similar<i class="material-icons right">search</i></a>
+          </div>
+        </div>`;
+          })
+          .join("\n");
+      } else {
+        //no cards
+        container.innerHTML = `<div class="card hoverable">
+          <div class="card-content">
+            <h3 class="card-title activator"><span>No Content Available.</span></h3>
+          </div>
+        </div>`;
+      }
+    }
+  },
 
   openDB() {
     let DBOpenReq = indexedDB.open("APP_DB", APP.dbVersion);
@@ -290,6 +206,7 @@ const APP = {
     DBOpenReq.addEventListener("success", (ev) => {
       APP.db = ev.target.result;
       console.log("FUYCK YEAH! DB success", APP.db);
+      APP.pageLoaded()
     });
 
     DBOpenReq.addEventListener("upgradeneeded", (ev) => {
@@ -309,11 +226,9 @@ const APP = {
         APP.db.createObjectStore("suggestionStore", { keyPath: "id" });
       }
     }),
-      //error listener
       DBOpenReq.addEventListener("error", (err) => {
         console.warn(err);
       });
-    //save db reference as APP.db
   },
 
   formSubmission(ev) {
@@ -332,7 +247,7 @@ const APP = {
     tx.oncomplete = (ev) => {
       console.log("On completed triggered", ev);
       window.location.href = `/results.html?keyword=${keyword}`;
-      // APP.buildList()
+      APP.checkDB(keyword)
     };
     tx.onerror = (err) => {
       console.warn(err);
--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //service worker for suggest-a-movie app
 
-let staticCache = 'static-v5';
-let dynamicCache = 'dynamic-v5';
+let staticCache = 'static-v1';
+let dynamicCache = 'dynamic-v1';
 // let dbVersion = 1;
 let cacheSize = 65;
 let staticList = [
